c95250e9eee7cfa8d0c61a9c82c445347ab75afb
Lee Stott <leestott@microsoft.com>
2020-02-29 06:33:21 -0800

    setup.sh
    install python libraries as user

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
 cd /usr/bin
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-pip install jupyter otter-grader datascience
+pip install jupyter otter-grader datascience --user
 echo -n "Ready to Go!" > /home/vsonline/workspace/"REFRESH NOW"
